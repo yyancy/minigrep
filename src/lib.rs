7ebe6639d583123653bb9385049eb4da220fd03c
yancy <dongyang@wondertek.com.cn>
2022-12-02 16:18:07 +0800

    add invert flag to select the unmatched text

--- src/lib.rs
+++ src/lib.rs
@@ -36,11 +36,11 @@ impl Config {
     }
 }
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
+    let contents = fs::read_to_string(&config.file_path)?;
     let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
+        search_case_insensitive(&config, &contents)
     } else {
-        search(&config.query, &contents)
+        search(&config, &contents)
     };
     for line in results {
         println!("{line}")
@@ -48,21 +48,27 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+pub fn search<'a>(config: &Config, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
     for line in contents.lines() {
-        if line.contains(query) {
+        let contained = line.contains(&config.query);
+        if contained && !config.invert {
+            results.push(line);
+        } else if config.invert{
             results.push(line);
         }
     }
     results
 }
 
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+pub fn search_case_insensitive<'a>(config: &Config, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
-    let query = query.to_lowercase();
+    let query = config.query.to_lowercase();
     for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
+        let contained = line.to_lowercase().contains(&query);
+        if contained && !config.invert {
+            results.push(line);
+        } else if config.invert{
             results.push(line);
         }
     }
@@ -80,7 +86,13 @@ mod tests {
 Rust:
 safe, fast, productive.
 Pick three.";
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+        let config = Config {
+            query: query.to_string(),
+            file_path: String::new(),
+            ignore_case: false,
+            invert: false,
+        };
+        assert_eq!(vec!["safe, fast, productive."], search(&config, contents))
     }
     #[test]
     fn case_insensitive() {
@@ -91,9 +103,15 @@ safe, fast, productive.
 Pick three.
 Trust me.";
 
+        let config = Config {
+            query: query.to_string(),
+            file_path: String::new(),
+            ignore_case: false,
+            invert: false,
+        };
         assert_eq!(
             vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
+            search_case_insensitive(&config, contents)
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 use clap::Parser;
 use minigrep::Config;
 use std::process;
+struct MyBox<T>(T);
 
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
 fn main() {
+    test();
     let config = Config::parse();
     println!("Search for {} in {}", config.query, config.file_path);
     if let Err(e) = minigrep::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(2)
     }
+}
+
+fn test() {
+ let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, y.0);
 }
